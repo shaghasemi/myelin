f5be1daf446481a87118277ab30eb54872fd0599
Hamed Shahghasemi <shaghasemi@gmail.com>
2025-02-26 12:45:45 +0330

    Deploy Flutter Web to GitHub Pages

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\Products\Myelin\myelin\.fvm\flutter_sdk"
+export "FLUTTER_APPLICATION_PATH=E:\Products\Myelin\myelin"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=2.3.3"
+export "FLUTTER_BUILD_NUMBER=233"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
